6e29bdd1b4ae1bbdee19478c4d316eff1feb7557
briandizon <bvdizon@gmail.com>
2021-03-20 07:25:11 +0800

    housekeeping

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,3 @@
-// importing User model
-const User = require('../models/user.js');
-
 // connecting to db
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/task-manager-api', {
